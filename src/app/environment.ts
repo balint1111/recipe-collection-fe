0a2434fe9e92c0abfe53422803fe1df159e59caf
Bálint Györffy <gyorffy.balint@hellopay.hu>
2024-05-14 14:40:46 +0200

    backend url change

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   // apiUrl: 'https://localhost:5001/api',
   // apiUrl: 'https://192.168.0.136:5001/api',
-  apiUrl: 'http://34.116.156.107:5001/api',
+  apiUrl: 'http://34.118.32.163:5001/api',
 };
